de3f369a9c9f6182b81bd12342000a78156f093c
ZergFlood <64033521+CarelTian@users.noreply.github.com>
2024-07-14 23:42:56 +1000

    Update db.ts

--- db.ts
+++ db.ts
@@ -1,10 +1,9 @@
 const mysql = require('mysql2');
 require('dotenv').config({ path: './db.env' });
-fuck
 function creatDB(){
     const connection = mysql.createConnection({
         host: process.env.DB_HOST,   
-        user: process.env.DB_USER, //a
+        user: process.env.DB_USER,
         password:process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
       });
