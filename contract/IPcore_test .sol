7e14ad7b36f9455fef4360c0486738cacaba956f
Sakuraking1232323 <148621652+Sakuraking1232323@users.noreply.github.com>
2024-07-18 20:46:36 +1000

    IPcore_test .sol

--- contract/IPcore_test .sol
+++ contract/IPcore_test .sol
@@ -67,10 +67,6 @@ contract ipCore is IPcore{
         return true;
     }
 
-    function checkFailure() public {
-        Assert.notEqual(uint(1), uint(1), "1 should not be equal to 1");
-    }
-
     /// Custom Transaction Context: https://remix-ide.readthedocs.io/en/latest/unittesting.html#customization
     /// #sender: account-1
     /// #value: 100
